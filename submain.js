e869ee81a64e93ccd8a04882c6ef0c2cc7c1d403
KoTeHK <132688013+KoTeHK@users.noreply.github.com>
2025-09-22 22:38:06 +0300

    Update submain.js

--- submain.js
+++ submain.js
@@ -5,4 +5,4 @@ const boom = getElementById("boom")
 boom.addEventListener("click", function() {
     window.open("https://docs.google.com/presentation/d/1QOyHLqTtyxDL4ENOm2gIjM7Gh-0EDrIi/edit?usp=sharing&ouid=117319882094475547455&rtpof=true&sd=true", "_blank");
 });
-}
+})
