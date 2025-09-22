116894f8c6bf2f4d9cd7f6fee34fe71b08090a15
KoTeHK <132688013+KoTeHK@users.noreply.github.com>
2025-09-22 22:31:58 +0300

    Create submain.js

---
+++ submain.js
@@ -0,0 +1,6 @@
+let bla = document.querySelector('.bla')
+
+const boom = getElementById("boom")
+boom.addEventListener("click", function() {
+    window.open("https://docs.google.com/presentation/d/1QOyHLqTtyxDL4ENOm2gIjM7Gh-0EDrIi/edit?usp=sharing&ouid=117319882094475547455&rtpof=true&sd=true", "_blank");
+});
